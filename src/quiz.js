cfb82441c546232da67999122d6ed7267b6c0a45
Richard Jarram <43706901+metalogica@users.noreply.github.com>
2024-05-02 22:07:24 -0400

    randomize

--- src/quiz.js
+++ src/quiz.js
@@ -12,6 +12,21 @@ const fadeInChoices = () => Array.from(
   )
 });
 
+const randomize = (originalArray) => {
+  const shuffledArray = [];
+  
+  do {
+    const randomInteger = Math.floor(Math.random() * originalArray.length);
+    if (shuffledArray.includes(randomInteger)) {
+      continue;
+    } else {
+      shuffledArray.push(randomInteger);
+    }
+  } while (shuffledArray.length < originalArray.length);
+
+  return shuffledArray.map(index => originalArray[index]);
+}
+
 const STATE = {
   NOT_STARTED: 0,
   IN_PROGRESS: 1,
@@ -36,58 +51,58 @@ const ROUNDS = [
   {
     question: `How many pounds of grapes are needed to make a bottle of wine?`,
     answer: `2.5 pounds`,
-    choices: [`10 pounds`, `2.5 pounds`, `61 pounds`, `0.7 pounds`]
+    choices: randomize([`10 pounds`, `2.5 pounds`, `61 pounds`, `0.7 pounds`])
   },
   {
     question: `The process by which grape sugar becomes alcohol is...`,
     answer: `Fermentation`,
-    choices: [`Carbonation`, `Fermentation`, `Yeastification`, `Ripening`],
+    choices: randomize([`Carbonation`, `Fermentation`, `Yeastification`, `Ripening`]),
   },
   {
     question: `What compound in red grapes makes you tired?`,
     answer: `Melatonin`,
-    choices: [`Alcohol level`, `Melatonin`, `Grape color`, `Sugar level`],
+    choices: randomize([`Alcohol level`, `Melatonin`, `Grape color`, `Sugar level`]),
   },
   {
     question: `Which of the following external factors does not influence a grape variety’s flavor profile?`,
     answer: `Bottle size`,
-    choices: [`Weather`, `Soil`, `Aging style and time`, `Bottle size`],
+    choices: randomize([`Weather`, `Soil`, `Aging style and time`, `Bottle size`]),
   },
   // surplus round
   // {
   //   question: `Which wine-making method gives wine a crisp and effervescent profile?`,
   //   answer: `Both Steel barrel aging AND Egg barrel aging`,
-  //   choices: [`Steel barrel aging`, `Oak barrel aging`, `Egg barrel aging`, `Both Steel barrel aging AND Egg barrel aging`],
+  //   choices: randomize([`Steel barrel aging`, `Oak barrel aging`, `Egg barrel aging`, `Both Steel barrel aging AND Egg barrel aging`]),
   // },
   {
     question: `Which country is not a top-five producer of wine?`,
     answer: `Chile`,
-    choices: [`Italy`, `Spain`, `Argentina`, `Chile`],
+    choices: randomize([`Italy`, `Spain`, `Argentina`, `Chile`]),
   },
   {
     question: `Which of the following is not one of the -S words of tasting?`,
     answer: `Stand`,
-    choices: [`Sip`, `Stand`, `Smell`, `Swirl`],
+    choices: randomize([`Sip`, `Stand`, `Smell`, `Swirl`]),
   },
   {
     question: `Which of these is the most diverse grape?`,
     answer: `Stand`,
-    choices: [`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`],
+    choices: randomize([`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`]),
   },
   {
     question: `Which of these is the most diverse grape?`,
     answer: `Chardonnay`,
-    choices: [`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`],
+    choices: randomize([`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`]),
   },
   {
     question: `Which wine is associated with flavours of green bell pepper, grass, and gooseberry?`,
     answer: `Sauvignon Blanc`,
-    choices: [`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`], 
+    choices: randomize([`Merlot`, `Cabernet Sauvignon`, `Chardonnay`, `Sauvignon Blanc`],) 
   },
   {
     question: `Where did winemaking begin?`,
     answer: `Georgia`,
-    choices: [`France`, `Argentina`, `Italy`, `Georgia`], 
+    choices: randomize([`France`, `Argentina`, `Italy`, `Georgia`],) 
   }    
 ];
 
