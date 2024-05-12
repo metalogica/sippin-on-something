836264008fe5e29b0c01a9f297f7636fb4106fe3
Richard Jarram <43706901+metalogica@users.noreply.github.com>
2024-05-11 20:39:07 -0400

    style: Update quiz layout and styles

--- src/quiz.js
+++ src/quiz.js
@@ -91,14 +91,14 @@ let CURRENT_ROUND = 0;
 document.addEventListener('alpine:init', () => {
   Alpine.store('quiz', {
     choiceLabels: ['A', 'B', 'C', 'D'],
-    competencyLevel: '2',
-    competencyText: 'fefefefefefef',
+    competencyLevel: '',
+    competencyText: '',
     guess: '',
     hasGuessed: false,
     currentRound: CURRENT_ROUND + 1,
     round: ROUNDS[CURRENT_ROUND],
-    score: 2,
-    state: STATE.COMPLETED,
+    score: 0,
+    state: STATE.NOT_STARTED,
 
     start () {
       this.state = STATE.IN_PROGRESS;
